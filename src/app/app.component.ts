b3a65d4f196c93eeec5025842b2d59970b4283c0
briankc804 <briankc804@gmail.com>
2022-04-09 15:49:28 +0300

    created a goal blueprint

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component,} from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  quotes:string[];
+
+  constructor(){
+    this.quotes=['i love quotes','buy whiskey', 'get a new phone']
+  }
+}
